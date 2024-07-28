86232f2a5b99768d283298adae09d5d41bfe4da7
Eddy Zheng <eddyzheng04@gmail.com>
2024-07-28 06:41:43 -0400

    finished instructions

--- frontend/src/Fire.js
+++ frontend/src/Fire.js
@@ -4,6 +4,9 @@ import settings from './settings.png';
 import exit from './exit.png';
 import yellow from './yellow.png';
 import hydrant from './hydrant.png';
+import suitcase from './case.png';
+import cross from './cross.png';
+import radar from './radar.png';
 
 function Fire({data}) {
   // display tasks here
@@ -14,7 +17,9 @@ function Fire({data}) {
             <div className="rightContainer">                
               <p className="title"> Instructions </p>
               <p className="title2">Based on the situation, here are some things you should do</p>
-                
+              <p className="instructions" style={{"position": "absolute", "top": "260px", "left": "90px"}}>euhfibdjdjnw</p>
+              <p className="instructions" style={{"position": "absolute", "top": "435px", "left": "90px"}}>fduhsiasjkduh</p>
+              <p className="instructions" style={{"position": "absolute", "top": "610px", "left": "90px"}}>fduhsiasjkduh</p>
             </div>
         </div>
         <img src={settings} alt="settings" className="settings"/>
@@ -22,6 +27,9 @@ function Fire({data}) {
         <img src={exit} alt="exit" className="exit"/>
         <img src={yellow} alt="yellow" className="triangle"/>
         <img src={hydrant} alt="hydrant" className="hydrant"/>
+        <img src={suitcase} alt="suitcase" className="icon1"/>
+        <img src={radar} alt="radar" className="icon2"/>
+        <img src={cross} alt="cross" className="icon3"/>
     </>
   );
 }
--- frontend/src/Police.js
+++ frontend/src/Police.js
@@ -4,6 +4,9 @@ import settings from './settings.png';
 import exit from './exit.png';
 import blue from './blue.png';
 import police from './police.png';
+import suitcase from './case.png';
+import cross from './cross.png';
+import radar from './radar.png';
 
 function Police({data}) {
   // display tasks here
@@ -14,6 +17,9 @@ function Police({data}) {
             <div className="rightContainer">
               <p className="title"> Instructions </p>
               <p className="title2">Based on the situation, here are some things you should do</p>
+              <p className="instructions" style={{"position": "absolute", "top": "260px", "left": "90px"}}>euhfibdjdjnw</p>
+              <p className="instructions" style={{"position": "absolute", "top": "435px", "left": "90px"}}>fduhsiasjkduh</p>
+              <p className="instructions" style={{"position": "absolute", "top": "610px", "left": "90px"}}>fduhsiasjkduh</p>
             </div>
         </div>
         <img src={settings} alt="settings" className="settings"/>
@@ -21,6 +27,9 @@ function Police({data}) {
         <img src={exit} alt="exit" className="exit"/>
         <img src={blue} alt="blue" className="triangle"/>
         <img src={police} alt="police" className="police"/>
+        <img src={suitcase} alt="suitcase" className="icon1"/>
+        <img src={radar} alt="radar" className="icon2"/>
+        <img src={cross} alt="cross" className="icon3"/>
     </>
   );
 }
