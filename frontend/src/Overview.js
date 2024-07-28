1ff174db1c52be832706dd5d73a2203387933af4
Fiona Cai <fiona.cai899@gmail.com>
2024-07-27 22:53:06 -0400

    overview

--- frontend/src/Overview.js
+++ frontend/src/Overview.js
@@ -1,6 +1,13 @@
 import React from 'react';
 import location from './location.png';
 import settings from './settings.png';
+import police from './police.png';
+import blue from './Ellipse 5.png';
+import health from './ambulance.png';
+import red from './Ellipse 6.png';
+import fire from './hydrant.png';
+import yellow from './Ellipse 7.png';
+
 import exit from './exit.png';
 
 function Overview() {
@@ -10,7 +17,18 @@ function Overview() {
         <div className="rootContainer">
             <div className="navbar"></div>
             <div className="rightContainer">
-                
+              <p className="title">Emergency overview</p>
+              <p className="title2">Here's a summary of the emergency.</p>
+              <p className="title2">We’ll give you the solution to their problem.</p>
+
+              <div className='addminus'>
+              <img src={blue} alt="blue" className="blueIcon" />
+              <img src={police} alt="police" className="policeIcon" />
+              <img src={red} alt="red" className="redIcon" />
+              <img src={health} alt="health" className="healthIcon" />
+              <img src={yellow} alt="yellow" className="yellowIcon" />
+              <img src={fire} alt="fire" className="fireIcon" />
+              </div>
             </div>
         </div>
         <img src={settings} alt="settings" className="settings"/>
