0870bff144457518f19d1fa080f3b992502042dc
Eddy Zheng <eddyzheng04@gmail.com>
2024-07-27 22:46:20 -0400

    finished instruction backgrounds

--- frontend/src/Medical.js
+++ frontend/src/Medical.js
@@ -2,6 +2,9 @@ import React from 'react';
 import location from './location.png';
 import settings from './settings.png';
 import exit from './exit.png';
+import red from './red.png';
+import ambulance from './ambulance.png';
+
 
 function Medical() {
   // display tasks here
@@ -11,12 +14,14 @@ function Medical() {
             <div className="navbar"></div>
             <div className="rightContainer">
                 <p className="title">Instructions</p>
-                <p className="subtitle">Based on the situation, here are some things you should do</p>
+                <p className="title2">Based on the situation, here are some things you should do</p>
             </div>
         </div>
         <img src={settings} alt="settings" className="settings"/>
         <img src={location} alt="location" className="location"/>
         <img src={exit} alt="exit" className="exit"/>
+        <img src={red} alt="red" className="triangle"/>
+        <img src={ambulance} alt="ambulance" className="ambulance"/>
     </>
   );
 }
